9bd5be3607ea87e9bede1a50ecf1a81ef7b84768
Jhonatan Ferreira <devjhow2003@gmail.com>
2024-05-05 23:01:02 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# first Task
+print(1+2+3+4+5)
+
+# second task
+
+media= (23+19+31)/3
+print(media)
+
+# third task
+print(403//73)
+
+# fourth task
+print(403%73)
+
---
+++ tasks2.py
@@ -0,0 +1,8 @@
+# first Task
+print(2**10)
+
+# second task
+print(abs(54-57))
+
+# third task
+print(min(34,29,31))
